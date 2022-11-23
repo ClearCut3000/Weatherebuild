655a52a40bc06f7b3b72073dc2e031885eaadbd4
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-23 23:06:51 +0300

    Initial Commit

---
+++ Weatherebuild/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weatherebuild
+//
+//  Created by Николай Никитин on 23.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weatherebuild/WeatherebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherebuildApp.swift
+//  Weatherebuild
+//
+//  Created by Николай Никитин on 23.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
