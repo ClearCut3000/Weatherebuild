347d428326b7d9a0005fa957040d6448192354a5
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-26 00:49:53 +0300

    Color extension

--- Weatherebuild/Color+Extension.swift
+++ Weatherebuild/Color+Extension.swift
@@ -7,12 +7,42 @@
 
 import SwiftUI
 
+/// Extension containing all background color that used in UI composition
 extension Color {
   static let midnightStart = Color(hue: 0.66, saturation: 0.8, brightness: 0.1)
-      static let midnightEnd = Color(hue: 0.62, saturation: 0.5, brightness: 0.33)
-      static let sunriseStart = Color(hue: 0.62, saturation: 0.6, brightness: 0.42)
-      static let sunriseEnd = Color(hue: 0.95, saturation: 0.35, brightness: 0.66)
-      static let sunnyDayStart = Color(hue: 0.6, saturation: 0.6, brightness: 0.6)
-      static let sunnyDayEnd = Color(hue: 0.6, saturation: 0.4, brightness: 0.85)
-      static let sunsetStart = Color.sunriseStart
-      static let sunsetEnd = Color(hue: 0.05, saturation: 0.34, brightness: 0.65)}
+  static let midnightEnd = Color(hue: 0.62, saturation: 0.5, brightness: 0.33)
+  static let sunriseStart = Color(hue: 0.62, saturation: 0.6, brightness: 0.42)
+  static let sunriseEnd = Color(hue: 0.95, saturation: 0.35, brightness: 0.66)
+  static let sunnyDayStart = Color(hue: 0.6, saturation: 0.6, brightness: 0.6)
+  static let sunnyDayEnd = Color(hue: 0.6, saturation: 0.4, brightness: 0.85)
+  static let sunsetStart = Color.sunriseStart
+  static let sunsetEnd = Color(hue: 0.05, saturation: 0.34, brightness: 0.65)
+}
+
+/// Extension for two methods: one for returning tuple with color components, other for interpolate that color components
+extension Color {
+  /// retrieved the RGBA values for a given Color
+  func getComponents() -> (red: Double, green: Double, blue: Double, alpha: Double) {
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+
+    let uiColor = UIColor(self)
+    uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+    return (red, green, blue, alpha)
+  }
+
+  /// knows how to blend any one Color with another Color based on some amount value
+  func interpolated(to other: Color, amount: Double) -> Color {
+    let componentsFrom = self.getComponents()
+    let componentsTo = other.getComponents()
+
+    let newRed = (1.0 - amount) * componentsFrom.red + (amount * componentsTo.red)
+    let newGreen = (1.0 - amount) * componentsFrom.green + (amount * componentsTo.green)
+    let newBlue = (1.0 - amount) * componentsFrom.blue + (amount * componentsTo.blue)
+    let newOpacity = (1.0 - amount) * componentsFrom.alpha + (amount * componentsTo.alpha)
+
+    return Color(.displayP3, red: newRed, green: newGreen, blue: newBlue, opacity: newOpacity)
+  }
+}
--- Weatherebuild/ContentView.swift
+++ Weatherebuild/ContentView.swift
@@ -14,10 +14,24 @@ struct ContentView: View {
   @State private var time = 0.0
 
   let backgroundTopStops: [Gradient.Stop] = [
-    .init(color: .sunriseStart, location: 0)
+    .init(color: .midnightStart, location: 0),
+    .init(color: .midnightStart, location: 0.25),
+    .init(color: .sunriseStart, location: 0.33),
+    .init(color: .sunnyDayStart, location: 0.38),
+    .init(color: .sunnyDayStart, location: 0.7),
+    .init(color: .sunriseStart, location: 0.78),
+    .init(color: .midnightStart, location: 0.82),
+    .init(color: .midnightStart, location: 1)
   ]
   let backgroundBottomStops: [Gradient.Stop] = [
-    .init(color: .sunriseEnd, location: 0)
+    .init(color: .midnightEnd, location: 0),
+    .init(color: .midnightEnd, location: 0.25),
+    .init(color: .sunriseEnd, location: 0.33),
+    .init(color: .sunnyDayEnd, location: 0.38),
+    .init(color: .sunnyDayEnd, location: 0.7),
+    .init(color: .sunsetEnd, location: 0.78),
+    .init(color: .midnightEnd, location: 0.82),
+    .init(color: .midnightEnd, location: 1)
   ]
 
   var formattedTime: String {
@@ -27,40 +41,40 @@ struct ContentView: View {
   }
 
   //MARK: - View Body
-    var body: some View {
-        ZStack {
-          CloudsView(thickness: .regular)
-        }
-        .preferredColorScheme(.dark)
-        .background( LinearGradient(colors: [
-          backgroundTopStops.interpolated(amount: time),
-          backgroundBottomStops.interpolated(amount: time)
-        ], startPoint: .top, endPoint: .bottom))
-        .safeAreaInset(edge: .bottom) {
-          VStack {
-            Text(formattedTime)
-              .padding(.top)
-            Picker("Thickness", selection: $cloudThickness) {
-              ForEach(Cloud.Thickness.allCases, id: \.self) {thickness in
-                Text(String(describing: thickness).capitalized)
-              }
-            }
-            .pickerStyle(.segmented)
-            HStack {
-              Text("Time:")
-              Slider(value: $time)
-            }
-            .padding()
-          }
-          .padding(5)
-          .frame(maxWidth: .infinity)
-          .background(.regularMaterial)
-        }
+  var body: some View {
+    ZStack {
+      CloudsView(thickness: .regular)
     }
+    .preferredColorScheme(.dark)
+    .background( LinearGradient(colors: [
+      backgroundTopStops.interpolated(amount: time),
+      backgroundBottomStops.interpolated(amount: time)
+    ], startPoint: .top, endPoint: .bottom))
+    .safeAreaInset(edge: .bottom) {
+      VStack {
+        Text(formattedTime)
+          .padding(.top)
+        Picker("Thickness", selection: $cloudThickness) {
+          ForEach(Cloud.Thickness.allCases, id: \.self) {thickness in
+            Text(String(describing: thickness).capitalized)
+          }
+        }
+        .pickerStyle(.segmented)
+        HStack {
+          Text("Time:")
+          Slider(value: $time)
+        }
+        .padding()
+      }
+      .padding(5)
+      .frame(maxWidth: .infinity)
+      .background(.regularMaterial)
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
