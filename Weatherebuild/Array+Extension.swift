249a09e59672909f5aba56df1e0b2271280cbe19
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-27 22:34:08 +0300

    Finalise array extension for color stops

--- Weatherebuild/Array+Extension.swift
+++ Weatherebuild/Array+Extension.swift
@@ -9,6 +9,28 @@ import SwiftUI
 
 extension Array where Element == Gradient.Stop {
     func interpolated(amount: Double) -> Color {
-        self[0].color
+      guard let initialStop = self.first else {
+             fatalError("Attempt to read color from empty stop array.")
+         }
+
+         var firstStop = initialStop
+         var secondStop = initialStop
+
+      for stop in self {
+          if stop.location < amount {
+              firstStop = stop
+          } else {
+              secondStop = stop
+              break
+          }
+      }
+      let totalDifference = secondStop.location - firstStop.location
+
+      if totalDifference > 0 {
+          let relativeDifference = (amount - firstStop.location) / totalDifference
+          return firstStop.color.interpolated(to: secondStop.color, amount: relativeDifference)
+      } else {
+          return firstStop.color.interpolated(to: secondStop.color, amount: 0)
+      }
     }
 }
