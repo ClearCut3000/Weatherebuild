93f8a073cd5552600e1a69384b2678cbe99f7cbc
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-28 23:55:00 +0300

    twinkling effect for stars

--- Weatherebuild/Stars/Star.swift
+++ Weatherebuild/Stars/Star.swift
@@ -11,10 +11,16 @@ class Star {
   var x: Double
   var y: Double
   var size: Double
+  var flickerInterval: Double
 
   init(x: Double, y: Double, size: Double) {
     self.x = x
     self.y = y
     self.size = size
+    if size > 2 && y < 250 {
+      flickerInterval = Double.random(in: 3...20)
+    } else {
+      flickerInterval = 0
+    }
   }
 }
--- Weatherebuild/Stars/StarsView.swift
+++ Weatherebuild/Stars/StarsView.swift
@@ -14,12 +14,34 @@ struct StarsView: View {
 
   //MARK: - View Body
     var body: some View {
-      TimelineView { timeline in
+      TimelineView(.animation) { timeline in
         Canvas { context, size in
+          let timeInterval = timeline.date.timeIntervalSince1970
           starField.update(date: timeline.date)
           context.addFilter(.blur(radius: 0.3))
           for (index, star) in starField.stars.enumerated() {
             let path = Path(ellipseIn: CGRect(x: star.x, y: star.y, width: star.size, height: star.size))
+            if star.flickerInterval > 0 {
+              /// makes flashing star
+              var flashLevel = sin(Double(index) + timeInterval * 4)
+              flashLevel = abs(flashLevel)
+              flashLevel /= 2
+              context.opacity = 0.5 + flashLevel
+            } else {
+              /// makes blooming star
+              var flashLevel = sin(Double(index) + timeInterval)
+              flashLevel *= star.flickerInterval
+              flashLevel -= star.flickerInterval - 1
+              if flashLevel > 0 {
+                  var contextCopy = context
+                  contextCopy.opacity = flashLevel
+                  contextCopy.addFilter(.blur(radius: 3))
+                  contextCopy.fill(path, with: .color(white: 1))
+                  contextCopy.fill(path, with: .color(white: 1))
+                  contextCopy.fill(path, with: .color(white: 1))
+              }
+              context.opacity = 1
+            }
             if index.isMultiple(of: 7) {
               context.fill(path, with: .color(red: 1, green: 0.85, blue: 0.8))
             } else {
