a22e9e94ba40354b3929bdceabe37b30032f94e7
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-29 22:43:57 +0300

    Added "generic" class for rainfall

---
+++ Weatherebuild/Storm.swift
@@ -0,0 +1,16 @@
+//
+//  Storm.swift
+//  Weatherebuild
+//
+//  Created by Николай Никитин on 29.11.2022.
+//
+
+import SwiftUI
+
+class Storm {
+    enum Contents: CaseIterable {
+        case none, rain, snow
+    }
+}
+
+
