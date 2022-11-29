90bdc1134c770a6d5efeb10a07fe1a31af9fe68f
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-30 01:36:17 +0300

    Implemented update method for storm class

---
+++ Weatherebuild/Storm/Storm.swift
@@ -0,0 +1,60 @@
+//
+//  Storm.swift
+//  Weatherebuild
+//
+//  Created by Николай Никитин on 29.11.2022.
+//
+
+import SwiftUI
+
+class Storm {
+  enum Contents: CaseIterable {
+    case none, rain, snow
+  }
+
+  //MARK: - Properties
+  var drops = [StormDrop]()
+  var lastUpdate = Date.now
+  var image: Image
+
+  //MARK: - Init
+  init(type: Contents, direction: Angle, strength: Int) {
+    switch type {
+    case .snow:
+      image = Image("snow")
+    default:
+      image = Image("rain")
+    }
+
+    for _ in 0..<strength {
+      drops.append(StormDrop(type: type, direction: direction + .degrees(90)))
+    }
+  }
+
+  //MARK: - Methods
+  func update(date: Date, size: CGSize) {
+    let delta = date.timeIntervalSince1970 - lastUpdate.timeIntervalSince1970
+    let divisor = size.height / size.width
+
+    for drop in drops {
+      let radians = drop.direction.radians
+      drop.x += cos(radians) * drop.speed * delta * divisor
+      drop.y += sin(radians) * drop.speed * delta
+
+      if drop.x < -0.2 {
+        drop.x += 1.4
+      }
+
+      if drop.y > 1.2 {
+        drop.x = Double.random(in: -0.2...1.2)
+        drop.y -= 1.4
+      }
+
+      drop.rotation += drop.rotationSpeed * delta
+    }
+
+    lastUpdate = date
+  }
+}
+
+
