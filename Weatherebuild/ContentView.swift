e4939fb619e3d9bd66c745c4b66820859a111fb8
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-03 23:02:00 +0300

    UI Controls toggle button added

--- Weatherebuild/ContentView.swift
+++ Weatherebuild/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
   @State private var lightningMaxBolts = 4.0
   @State private var lightningForkProbability = 20.0
 
+  @State private var showingControls = true
+
   let backgroundTopStops: [Gradient.Stop] = [
     .init(color: .midnightStart, location: 0),
     .init(color: .midnightStart, location: 0.25),
@@ -41,36 +43,36 @@ struct ContentView: View {
     .init(color: .midnightEnd, location: 1)
   ]
   let cloudTopStops: [Gradient.Stop] = [
-      .init(color: .darkCloudStart, location: 0),
-      .init(color: .darkCloudStart, location: 0.25),
-      .init(color: .sunriseCloudStart, location: 0.33),
-      .init(color: .lightCloudStart, location: 0.38),
-      .init(color: .lightCloudStart, location: 0.7),
-      .init(color: .sunsetCloudStart, location: 0.78),
-      .init(color: .darkCloudStart, location: 0.82),
-      .init(color: .darkCloudStart, location: 1)
+    .init(color: .darkCloudStart, location: 0),
+    .init(color: .darkCloudStart, location: 0.25),
+    .init(color: .sunriseCloudStart, location: 0.33),
+    .init(color: .lightCloudStart, location: 0.38),
+    .init(color: .lightCloudStart, location: 0.7),
+    .init(color: .sunsetCloudStart, location: 0.78),
+    .init(color: .darkCloudStart, location: 0.82),
+    .init(color: .darkCloudStart, location: 1)
   ]
 
   let cloudBottomStops: [Gradient.Stop] = [
-      .init(color: .darkCloudEnd, location: 0),
-      .init(color: .darkCloudEnd, location: 0.25),
-      .init(color: .sunriseCloudEnd, location: 0.33),
-      .init(color: .lightCloudEnd, location: 0.38),
-      .init(color: .lightCloudEnd, location: 0.7),
-      .init(color: .sunsetCloudEnd, location: 0.78),
-      .init(color: .darkCloudEnd, location: 0.82),
-      .init(color: .darkCloudEnd, location: 1)
+    .init(color: .darkCloudEnd, location: 0),
+    .init(color: .darkCloudEnd, location: 0.25),
+    .init(color: .sunriseCloudEnd, location: 0.33),
+    .init(color: .lightCloudEnd, location: 0.38),
+    .init(color: .lightCloudEnd, location: 0.7),
+    .init(color: .sunsetCloudEnd, location: 0.78),
+    .init(color: .darkCloudEnd, location: 0.82),
+    .init(color: .darkCloudEnd, location: 1)
   ]
 
   let starStops: [Gradient.Stop] = [
-      .init(color: .white, location: 0),
-      .init(color: .white, location: 0.25),
-      .init(color: .clear, location: 0.333),
-      .init(color: .clear, location: 0.38),
-      .init(color: .clear, location: 0.7),
-      .init(color: .clear, location: 0.8),
-      .init(color: .white, location: 0.85),
-      .init(color: .white, location: 1)
+    .init(color: .white, location: 0),
+    .init(color: .white, location: 0.25),
+    .init(color: .clear, location: 0.333),
+    .init(color: .clear, location: 0.38),
+    .init(color: .clear, location: 0.7),
+    .init(color: .clear, location: 0.8),
+    .init(color: .white, location: 0.85),
+    .init(color: .white, location: 1)
   ]
 
   var starOpacity: Double {
@@ -87,22 +89,22 @@ struct ContentView: View {
   //MARK: - View Body
   var body: some View {
     ZStack {
-//      StarsView()
-//        .opacity(starOpacity)
-//
-//      CloudsView(thickness: cloudThickness,
-//                 topTint: cloudTopStops.interpolated(amount: time),
-//                 bottomTint: cloudBottomStops.interpolated(amount: time))
+      //      StarsView()
+      //        .opacity(starOpacity)
+      //
+      //      CloudsView(thickness: cloudThickness,
+      //                 topTint: cloudTopStops.interpolated(amount: time),
+      //                 bottomTint: cloudBottomStops.interpolated(amount: time))
 
       LightningView(maximumBolts: Int(lightningMaxBolts), forkProbability: Int(lightningForkProbability))
 
-//      if stormType != .none {
-//        StormView(type: stormType, direction: .degrees(rainAngle), strength: Int(rainIntensity))
-//      }
-//
-//      WeatherDetailsView(tintColor: backgroundTopStops.interpolated(amount: time),
-//                         residueType: stormType,
-//                         residueStrength: rainIntensity)
+      //      if stormType != .none {
+      //        StormView(type: stormType, direction: .degrees(rainAngle), strength: Int(rainIntensity))
+      //      }
+      //
+      //      WeatherDetailsView(tintColor: backgroundTopStops.interpolated(amount: time),
+      //                         residueType: stormType,
+      //                         residueStrength: rainIntensity)
     }
     .preferredColorScheme(.dark)
     .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -112,50 +114,59 @@ struct ContentView: View {
     ], startPoint: .top, endPoint: .bottom))
     .safeAreaInset(edge: .bottom) {
       VStack {
-        Text(formattedTime)
-          .padding(.top)
-        Picker("Thickness", selection: $cloudThickness) {
-          ForEach(Cloud.Thickness.allCases, id: \.self) {thickness in
-            Text(String(describing: thickness).capitalized)
+        Button("Toggle controls") {
+          withAnimation {
+            showingControls.toggle()
           }
         }
-        .pickerStyle(.segmented)
-        HStack {
-          Text("Time:")
-          Slider(value: $time)
-        }
-        .padding()
+        if showingControls {
+          VStack {
+            Text(formattedTime)
+              .padding(.top)
+            Picker("Thickness", selection: $cloudThickness) {
+              ForEach(Cloud.Thickness.allCases, id: \.self) {thickness in
+                Text(String(describing: thickness).capitalized)
+              }
+            }
+            .pickerStyle(.segmented)
+            HStack {
+              Text("Time:")
+              Slider(value: $time)
+            }
+            .padding()
 
-        Picker("Presipitation", selection: $stormType) {
-          ForEach(Storm.Contents.allCases, id: \.self) { stormType in
-            Text(String(describing: stormType).capitalized)
+            Picker("Presipitation", selection: $stormType) {
+              ForEach(Storm.Contents.allCases, id: \.self) { stormType in
+                Text(String(describing: stormType).capitalized)
+              }
+            }
+            .pickerStyle(.segmented)
+
+            HStack {
+              Text("Intensity:")
+              Slider(value: $rainIntensity, in: 0...1000)
+            }
+            .padding(.horizontal)
+
+            HStack {
+              Text("Angle:")
+              Slider(value: $rainAngle, in: 0...90)
+            }
+
+            HStack {
+              Text("Max Bolts:")
+              Slider(value: $lightningMaxBolts, in: 0...10)
+            }
+            .padding(.horizontal)
+
+            HStack {
+              Text("Fork %:")
+              Slider(value: $lightningForkProbability, in: 0...100)
+            }
+            .padding(.horizontal)
           }
+          .transition(.move(edge: .bottom))
         }
-        .pickerStyle(.segmented)
-
-        HStack {
-          Text("Intensity:")
-          Slider(value: $rainIntensity, in: 0...1000)
-        }
-        .padding(.horizontal)
-
-        HStack {
-          Text("Angle:")
-          Slider(value: $rainAngle, in: 0...90)
-        }
-
-        HStack {
-          Text("Max Bolts:")
-          Slider(value: $lightningMaxBolts, in: 0...10)
-        }
-        .padding(.horizontal)
-
-        HStack {
-          Text("Fork %:")
-          Slider(value: $lightningForkProbability, in: 0...100)
-        }
-        .padding(.horizontal)
-
       }
       .padding(5)
       .frame(maxWidth: .infinity)
